c96463e6ec59186793b48dc8f62fa09468e6e9a3
ajay1264 <kambleajay146@gmail.com>
2024-07-11 10:57:56 +0000

    container config done

---
+++ 01_Bascis/variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "ajayroy123@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"  // aisa likhna avoid kare humesh ek variable add kijiye 
+
+console.log(accountId);
+// instead we can use diif method
+
+console.table([accountId , accountEmail , accountPassword]);
+/* var mat use kyuki woh block scope and functional scope main problem deta hai */
+// agar variable ko value nhi dete toh woh undefined hoti hai 
