95f4c4aa2f4688c320cd225506d4454a3e2dab0a
ajay1264 <kambleajay146@gmail.com>
2024-07-11 11:16:45 +0000

    container config done

---
+++ 01_Bascis/datatypes.js
@@ -0,0 +1,22 @@
+// "use strict"; yeh syntax se code ko as a newer version treaat kiya jayega
+// alert(3+3) error show karega kyuki ye node main run ho rah hai browser main nhi 
+//ecma script documentation original tc39 aur mdn
+// lets discuss datatype
+
+let name = "ajay"
+let age = 22 
+let isLoggedIn = false
+
+
+// number ka range bhi hai 2 to power 53 usually big websit like reddit trading main big int use hota hai 
+//bigint
+//string => " "
+//boolean => true/false
+//null => standalone value
+//undefined => abhi value assigned nhi hui hai 
+//symbol => unique ke liye hum symbol use karte hai 
+
+
+//object
+//jab bhi null ko execute karte hai tab uski value object hoti hai 
+//aur jab undefined ko execute karenge tab uski value undefined hi rehti hai 
