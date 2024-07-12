b1cdcd20570a04fdc241a6810845c1c7a149ac02
ajay1264 <kambleajay146@gmail.com>
2024-07-12 14:22:04 +0530

    ops done

---
+++ 01_Bascis/operation.js
@@ -0,0 +1,46 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+//operations 
+//negagtive value assigned ki hai toh negve hi ayege 
+let value = 3 
+let negValue = -value 
+//console.log(negValue); 
+
+/* basci ops 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+ */ 
+
+//how to join a string 
+let str1 = "hello"
+let str2 = "ajay"
+
+let str3 = str2 + str1
+//console.log(str3);
+
+//some complex things in ops 
+
+/*console.log("1"+2); //in this "1" is string and second is number is 2
+console.log(1 + "2"); //in this "2" is string so the answer is will be 12 
+console.log("1" + 2 + 2); //in this "1" is string and 2+2=4 but the answer is diff it is 122
+console.log(1 + 2 +"2"); //in this same dif situation as above but the ans will be 32
+// to find this ecma script havea rule 
+// in toprimitive 
+
+
+console.log(true); in this output will be true 
+console.log(+true); isme output 1 hoga 
+console.log(true+); isme output error ayega */
+
+
+//learn more about this in mdn operators
+let gameCounter = 100
+++gameCounter; //prefix
+gameCounter++; //post 
+console.log(gameCounter);
