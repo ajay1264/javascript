aed8e22dc15c73bf40846f14ab54e3515705c285
ajay1264 <kambleajay146@gmail.com>
2024-07-13 20:28:54 +0530

    objects

---
+++ 02_Basics/object_indepth.js
@@ -0,0 +1,44 @@
+//object : object ek data structure ahe jyaat data ekatr thevla jaato 
+
+// singleton 
+// object.create is also a way to create a object 
+
+//object literals 
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name:"Ajay",
+    "full name": "ajay roy",
+    age: 18,
+    [mySym]: "mykey1",
+    email: "ajay123@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["monday", "sunday"]
+}
+
+//console.log(JsUser.email);   //this is not a good way 
+//console.log(JsUser["email"]); //usually use this 
+//console.log(JsUser["full name"]);
+//console.log(JsUser[mySym]);
+
+//change the value of the object
+JsUser.email = "ajajy@gmail.com"  //old email id change into new id 
+//console.log(JsUser);
+//yet we can also freeze the email id 
+//Object.freeze(JsUser)     //this will stop the object to change and will not make any change 
+JsUser.email = "roy@.com"
+//console.log(JsUser);
+
+
+
+
+
+// now to add a function in object 
+
+JsUser.greeting(){
+      console.log("hello world ");
+
+}
+console.log(JsUser.greeting);
