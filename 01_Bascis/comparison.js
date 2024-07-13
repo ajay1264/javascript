afe419211b64b2ee8dec953a7f197b17c6ff0c10
ajay1264 <kambleajay146@gmail.com>
2024-07-13 09:24:01 +0000

    comapre

--- 01_Bascis/comparison.js
+++ 01_Bascis/comparison.js
@@ -1,3 +1,4 @@
+/*
 // basic comparison 
 /* 
 console.log(2 > 1);
@@ -7,12 +8,18 @@ console.log(2 == 1);
 console.log(2 != 1);
 */
 
+
+//console.log("2" > 1);
+//console.log("02" > );
+
+
+
 //console.log("2" > 1); //ther are 2 diff data type as its not easy for comparing 
 //console.log("02" > 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >= 0);
 
 /*the reason is that an equality check == and comparison 
 ><>= work differently c
