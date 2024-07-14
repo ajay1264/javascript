49723d1b01ee418f5af1ca3e1a419359a7ed1d2e
ajay1264 <kambleajay146@gmail.com>
2024-07-15 01:39:11 +0530

    functions

---
+++ 03_Basics/functions.js
@@ -0,0 +1,42 @@
+//functions
+/*
+console.log("A");
+console.log("j");
+console.log("a");
+console.log("y");
+
+instead of these we can use function to call them at once 
+
+
+//how to declare a function 
+
+function sayMyName() {
+    console.log("A");
+    console.log("j");
+    console.log("a");
+console.log("y");
+
+    
+}
+
+sayMyName()   // to execute
+*/
+
+//basics function name(paraameters) after the name they are called as parameters 
+// and after the console where the function will be call in the bracket is called arguement
+//function to add numbers 
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+
+}
+addTwoNumbers(3,4)
+
+//this is normal calc but what  is someone put string  instead of number 
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+
+}
+addTwoNumbers(3,"4")
+
