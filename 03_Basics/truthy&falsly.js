93f244b6cec105523eff70f34fad16562a3d3364
ajay1264 <kambleajay146@gmail.com>
2024-07-20 13:53:17 +0530

    truthy&falsly

---
+++ 03_Basics/truthy&falsly.js
@@ -0,0 +1,61 @@
+
+//   --------------- Truthy&false ------------ truthy.js
+
+//exp
+
+/*
+const userEmail = "roy@gmail.com"    //as you can see that it is showing that he go the email where in the "roy..."
+//but if put a empty string it will say dont have email as if we put a array "[]" it will show it has email as it shows that it is truthy 
+if (userEmail) {
+    console.log("got email");        
+} else {
+    console.log("dont have email");
+}
+
+
+
+//falsy values
+
+//false , 0 , -0 , BigInt 0n, "" , null, undefined, NaN
+
+// truthy values
+
+// "0", 'false', " ", [], {}, function(){}
+
+
+
+// As you can see that array also has truthy value where if there is nothing in the array stil it shows true here is the way to check the array truthy situation
+
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
+
+
+//how to know that the object is empty or not 
+
+const emptyObj = {}
+ 
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is empty");
+}
+
+
+
+//imp note
+false == 0 => true
+false == '' => true
+0 == '' => true
+
+
+
+//  Nullish Coalescing Operator (??)  (main symbols are null and undefined)
+
+let val1;
+//val = 5 ?? 10      //as in o/p it will be 5 as it will give the first value but in big database it gives two values imagine if there values didnt defined it will make more chaos 
+//val1 = null ?? 10    //in this it will not take the first value
+//val1 = undefined ?? 15  
+val1 = null ?? 10 ?? 20
+console.log(val1);
+
+*/
+
