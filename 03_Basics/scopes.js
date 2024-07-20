928088ec53b03d74d4e30693f3662e55f9457102
ajay1264 <kambleajay146@gmail.com>
2024-07-20 13:46:23 +0530

    scopes1&2

--- 03_Basics/scopes.js
+++ 03_Basics/scopes.js
@@ -1,4 +1,37 @@
- //  *******SCOPES IN JS 2*******   
+
+          // Scopes in Js //
+
+//scopes  has let var and const
+//  {}  this are scopes and dont mix it up with obj "when it will come with function or if else " to usko bola jata hai scope of if else etc
+
+if(true) {
+    let a = 10
+    var b = 20
+    const c = 30   //if you declare it by c=30 then also it will be same 
+}
+//console.log(a);
+//console.log(b);
+//console.log(c);
+//if we run this a and c will say not defined but the var will give the value
+// as it will create chaos while if another person create one more a=20 it will change the value of the before run a 
+
+
+
+//udersting global scope 
+let a = 300   //it is called as global scope it can access by anywhere
+
+if(true) {
+    let a = 10
+    var b = 20
+    const c = 30
+    console.log("INNER:", a);  //as you can see that the inside value didnt change it is as it is and is called block scope 
+}
+console.log(a);   //as we will declare a it give the output of the a which is global
+
+
+// scope in windows  and code environment jo scope hota hai woh dono alag hota hai 
+
+//  *******SCOPES IN JS 2*******   
 
 // nested scopes 
 
