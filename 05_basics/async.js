d1621e47fa391a88a7495536b0a1be664bfe89ff
ajay1264 <kambleajay146@gmail.com>
2024-07-21 19:25:41 +0530

    asyndiagram

---
+++ 05_basics/async.js
@@ -0,0 +1,22 @@
+//async code
+/*
+1)javascript is a synchronus language that is a code after code will run }
+2)it is a single threaded language                                       }  this is a default js behavuiour 
+
+EXECUTION CONTEXT 
+1)executes code one by one
+log=>1
+log=>2  each operation will act as the first one run
+
+1] Blocking code            vs       Non blocking code 
+-block the flow of the code          -do not block the code
+-read file sync                      -read file async
+
+
+
+task que makes the code faster
+
+
+jab bhi program execute hota hai tab uska call stack banta hai
+
+*/
